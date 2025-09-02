22a001244937d8bb4d848e7b8c1452a00599068a
a <94448096+bcur1ous@users.noreply.github.com>
2025-09-02 11:03:56 +0800

    Update 1.py

--- a/1.py
+++ a/1.py
@@ -1,2 +1,4 @@
 access_key_id = LTAI516jC15LJpjS2Gi3iJa1
 access_key_secret = abcdefghijklmnopqrstuvwxyz1234 
+AWS_ACCESS_KEY_ID = "AKIAIMNOFAKE7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"'
